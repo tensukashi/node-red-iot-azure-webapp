a8e0810fc1b22c0106d037401c5a316926641766
mdvanes <4253562+mdvanes@users.noreply.github.com>
2022-05-25 15:02:23 +0200

    Update postinstall.cmd

--- postinstall.cmd
+++ postinstall.cmd
@@ -2,6 +2,7 @@
 (if not exist \home\nodered mkdir \home\nodered)
 (if not exist \home\nodered\.node-red mkdir \home\nodered\.node-red)
 copy npm_.cmd \home\nodered\.node-red\npm_.cmd
+copy flows.json \home\nodered\.node-red\flows.json
 pushd \home\nodered\.node-red
 npm install node-red-dashboard node-red-contrib-azureiothubnode node-red-contrib-cognitive-services node-red-contrib-azure-blob-storage node-red-contrib-azure-table-storage node-red-contrib-azure-sql node-red-contrib-azure-documentdb node-red-contrib-azureiotc2d node-red-node-swagger & (
      copy npm_.cmd npm.cmd /y
