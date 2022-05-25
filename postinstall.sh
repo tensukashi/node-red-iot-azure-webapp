812bb7559b9a436a66f4dc1224a8533732ae6352
mdvanes <4253562+mdvanes@users.noreply.github.com>
2022-05-25 20:07:46 +0200

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -5,5 +5,5 @@ chmod 777 /home/nodered
 mkdir /home/nodered/.node-red
 chmod 777 /home/nodered/.node-red
 cd /home/nodered/.node-red
-cp /home/site/wwwroot/flows.json flows.json
+cp -R /home/site/wwwroot/src/* .
 npm install node-red-dashboard node-red-contrib-azureiothubnode node-red-contrib-cognitive-services node-red-contrib-azure-blob-storage node-red-contrib-azure-table-storage node-red-contrib-azure-sql node-red-contrib-azure-documentdb node-red-contrib-azureiotc2d node-red-node-swagger
